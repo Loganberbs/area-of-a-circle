0e5de7d54c1c9e4cec787375adab1eec4be8a412
Loganberbs <58014408+Loganberbs@users.noreply.github.com>
2020-09-29 21:02:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace areaofcircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Area of a Circle - 2.6.1
+            Console.WriteLine("Enter a radius: ");
+            string radiusInput = Console.ReadLine();
+            double radius = double.Parse(radiusInput);
+            double area = Math.PI * radius * radius;
+            Console.WriteLine($"The area of circle with {radius} radius is {area}.");
+
+            // More calculations - 2.6.2
+            double circumference = 2 * Math.PI * radius;
+            Console.WriteLine($"The circumference of a circle with a radius of {radius} is {circumference}");
+            Console.WriteLine($"The diameter of a circle with a radius of {radius} is {radius * 2}");
+                
+            //Road Trip
+            Console.WriteLine("What is the MPG of your car?");
+            double mpg = double.Parse(Console.ReadLine());
+            double gallons = circumference / mpg;
+            Console.WriteLine($"Your vehicle has a fuel efficency rate of {mpg}mpg ");
+
+
+
+
+        }
+    }
+}
